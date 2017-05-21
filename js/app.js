8c45503fdceca8110ce393709c67ab621d1d4004
phuchieuct <phuchieuct93@gmail.com>
2017-05-21 19:07:47 +0700

    Update

--- js/app.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module("app",[]);
-app.controller = app.controller("controller",function($scope){
-    $scope.name =JSON.parse( window.localStorage.getItem("name")).name||"My name should be here";
-    $scope.changeModel = function(){
-       window.localStorage.setItem("name",JSON.stringify({name:$scope.name}));
-  
-        
-    };
-});
